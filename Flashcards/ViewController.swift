f2e29359be459a63307d89bc774e68805a7fabaa
Denize Ignacio <dignacio@denizes-macbook-pro.local>
2018-10-16 13:01:49 -0700

    Simple Flashcards App Optionals 1 & 2 So Far

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     @IBOutlet weak var FrontLabel: UILabel!
     @IBOutlet weak var BackLabel: UILabel!
+    @IBOutlet weak var card: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        card.layer.cornerRadius = 20.0
+        card.clipsToBounds = true
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.2
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
-        FrontLabel.isHidden = true
+        if( FrontLabel.isHidden == true){
+            FrontLabel.isHidden = false
+        }
+        else if( FrontLabel.isHidden == false)
+        {
+            FrontLabel.isHidden = true
+        }
     }
     
 }
